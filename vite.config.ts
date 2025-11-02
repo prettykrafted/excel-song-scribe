7b38484ea2bc53d5ede745c2c77cf66f81652f6e
Nkosinathi Ngwenya <140812018+nkosi-Macenge@users.noreply.github.com>
2025-11-02 19:03:48 +0200

    using hash router

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/excel-song-scribe/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
